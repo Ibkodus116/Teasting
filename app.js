cea40f5b362e4757ffbfdc46a76f63822c017223
akinlope <akinrimisitt@gmail.com>
2022-05-13 13:00:16 +0100

    Test push

---
+++ app.js
@@ -0,0 +1,31 @@
+// let array = [];
+// let fName = document.getElementById("fName")
+// let sName = document.getElementById("sName")
+// let btn = document.getElementById("submit")
+
+let nArr = [];
+nArr.push(9)
+
+
+
+// btn.addEventListener("click", (e) =>{
+//     e.preventDefault();
+    
+//     if (fName.value == "" || sName.value == "") {
+    //         alert("Text filled Can't be empty")
+    //     }else{
+        //         array.push({
+            //             firstName:fName.value,
+            //             lastName:sName.value
+            //         })
+            
+            //         console.log(array);
+//     }
+//     fName.value = ""
+//     sName.value = ""
+
+
+let raNdom = Math.floor(Math.random()*(15-10)+10)
+
+nArr.push(raNdom)
+console.log(raNdom);
